064510523157196cb28aafb448d799626a98b597
Roman Zaytsev <r.zaytsev@me.com>
2013-08-26 16:31:25 +0400

    second commit

--- app.py
+++ app.py
@@ -2,38 +2,64 @@
 # coding='utf-8'
 
 import os
-from flask import Flask, redirect, render_template, session, escape, url_for, flash
+from flask import Flask, render_template
 from werkzeug.contrib.fixers import  ProxyFix
-
+import redis
 
 app = Flask(__name__)
 app.debug = True
 
 photo_dir = 'static/photo/'
 
-@app.route("/")
+@app.route('/')
 def start():
-    return render_template('page.html', gals = read_photos_from_disk() )
+    return render_template('page.html', gals = read_photos_from_disk(), edit='', msg='' )
+
+@app.route('/admin/<key>')
+def start_admin(key):
+    for f in os.listdir(photo_dir):
+        if f == 'key.txt':
+            with open(photo_dir  + 'key.txt') as f1:
+                stored_key = f1.read().strip()
+    editable ='tt'
+    error = 'ok'
+    if key == stored_key:
+        editable = 'editText'
+    else:
+         error = key + ' != ' + stored_key
+
+    return render_template('page.html', gals = read_photos_from_disk(), edit = editable , msg=error)
+
+
+
+@app.route('/edit/<type>/<val1>')
+def edit_comment(type,val1):
+    r = redis.StrictRedis(host="localhost",port=6379,db=1)
+    text = val1
+    if text == " ":
+        text = 'empty'
+    r.set(type + ':' + (val1.split("&")[:-1])[0].strip(), (val1.split("&")[:-1])[1].strip())
+    return  (val1.split("&")[:-1])[1].strip()
+
+
 
 def read_photos_from_disk():
+    r = redis.StrictRedis(host="localhost",port=6379,db=1)
     gals = []
     for dir in os.listdir(photo_dir):
         gallery = {}
         if not os.path.isfile(photo_dir + dir):
+            gallery['id'] = dir
             gallery['name'] = dir
-            gallery['description'] = ''
-            gallery['captions'] = ''
+            gallery['description'] = 'empty'
             gallery['photos'] = []
             for f in os.listdir(photo_dir + dir):
-                if os.path.isfile(photo_dir + dir + '/' + f) and f == 'descr.txt':
-                    with open(photo_dir + dir + '/' + f) as f1:
-                        gallery['description'] = f1.read()
-                if os.path.isfile(photo_dir + dir + '/' + f) and f == 'name.txt':
-                    with open(photo_dir + dir + '/' + f) as f1:
-                        gallery['name'] = f1.read()
-                if os.path.isfile(photo_dir + dir + '/' + f) and f == 'captions.txt':
-                    with open(photo_dir + dir + '/' + f) as f1:
-                        gallery['captions'] = f1.read()
+                gallery_name = r.get('name:'+dir)
+                if gallery_name:
+                    gallery['name'] = gallery_name
+                gallery_description = r.get('description:'+dir)
+                if gallery_description:
+                    gallery['description'] = gallery_description
 
                 if os.path.isfile(photo_dir + dir + '/' + f) and f.endswith(".jpg"):
                     gallery['photos'].append('/' + photo_dir + dir + '/' + f)
